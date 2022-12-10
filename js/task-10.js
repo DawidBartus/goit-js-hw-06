252ab5200e430abd62dea763eaae63c0a3e743ba
Dawid <davidowsky9519@gmail.com>
2022-12-10 11:07:30 +0100

    fix

--- js/task-10.js
+++ js/task-10.js
@@ -2,29 +2,19 @@ const btnCreate = document.querySelector("[data-create]");
 const btnDestroy = document.querySelector("[data-destroy]");
 const input = document.querySelector('[type="number"]');
 const divBox = document.querySelector("#boxes");
+let startVal = 30;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const chceckStartValue = () => {
-  if (divBox.hasChildNodes() === true) {
-    let value = parseInt(divBox.lastChild.style.width) + 10;
-    return value;
-  } else {
-    let value = 30;
-    return value;
-  }
-};
-
 const createBoxes = () => {
   let amount = input.value;
-  let startValue = chceckStartValue();
+
   if (amount <= 100) {
-    console.log(startValue);
     for (let i = 0; i < input.value; i++) {
       const newDiv = document.createElement("div");
-      const size = startValue + 10 * i;
+      const size = startVal + 10 * i;
       newDiv.style.width = size + "px";
       newDiv.style.height = size + "px";
       newDiv.style.backgroundColor = getRandomHexColor();
@@ -33,12 +23,16 @@ const createBoxes = () => {
   } else {
     alert("You don't pay me enough for this.");
   }
+  startVal = divBox.childNodes.length * 10 + 30;
+  return startVal;
 };
 
 const destroyBoxes = () => {
   while (divBox.firstChild) {
     divBox.removeChild(divBox.firstChild);
   }
+  startVal = 30;
+  return startVal;
 };
 
 btnCreate.addEventListener("click", createBoxes);
